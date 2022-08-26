91f8b4b20b670e79b347e3868db022a6eef58c97
HENRIQUE GUILHERME ESTRELA <henestrela@hotmail.com>
2022-08-25 20:15:10 -0400

    Entrega-1

--- Src/Dataset.py
+++ Src/Dataset.py
@@ -3,7 +3,8 @@ import os
 
 dirname = os.path.dirname(__file__)
 
-datasetfull = pd.concat([pd.read_csv(dirname +'/datas/reclamacoes-fundamentadas-sindec-2012.csv'),
+datasetfull = pd.concat([
+          pd.read_csv(dirname +'/datas/reclamacoes-fundamentadas-sindec-2012.csv'),
           pd.read_csv(dirname +'/datas/reclamacoes-fundamentadas-sindec-2013.csv', low_memory=False),
           pd.read_csv(dirname +'/datas/reclamacoes-fundamentadas-sindec-2014.csv'),
           pd.read_csv(dirname +'/datas/reclamacoes-fundamentadas-sindec-2015.csv', low_memory=False),
