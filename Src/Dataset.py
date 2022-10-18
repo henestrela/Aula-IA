6a9c10c2acf900b32538a8ca06a10ba94258d2d8
HENRIQUE GUILHERME ESTRELA <henestrela@hotmail.com>
2022-10-17 20:27:46 -0400

    Entrega

--- Src/Dataset.py
+++ Src/Dataset.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import os
+from nltk.tokenize import word_tokenize, sent_tokenize
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords 
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem import PorterStemmer
+st = PorterStemmer()
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+nltk.download('punkt')
 
 dirname = os.path.dirname(__file__)
 
@@ -26,3 +38,54 @@ print('Shape:')
 print(datasetFullAfterSave.shape)
 print('\n\nColumns')
 print(datasetFullAfterSave.columns)
+
+
+#Diminuindo palavras com Stemming
+
+
+print("Diminuindo palavras com Stemmin:")
+stem = st.stem(datasetFull['NomeFantasiaRFB'].str.lower().str.cat(sep=' '))
+print(len(stem))
+
+#pegando os dados da planilha em lower case
+tokensNomeFantasia = word_tokenize(stem)
+
+print("Total de tokens inicias:")
+print(len(tokensNomeFantasia))
+
+#remove stopwords
+
+stopwords = stopwords.words('portuguese')
+
+res = [i for i in tokensNomeFantasia if i not in stopwords]
+
+print("Total de tokens inicias apos a limpeza do stopwords:")
+print(len(res))
+
+#remove stopwords
+setTokensNomeFantasia = set(res)
+
+print("Total de tokens inicias apos o set:")
+print(len(setTokensNomeFantasia))
+
+
+vectorizer = CountVectorizer() 
+
+X = vectorizer.fit_transform(setTokensNomeFantasia)
+
+#print(vectorizer.vocabulary_)
+
+print("Total apos limpezas:")
+print(len(vectorizer.vocabulary_))
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 100)
+
+
+
+tfidf = TfidfVectorizer()
+
+tfidf_matrix = tfidf.fit_transform(setTokensNomeFantasia)
+print(pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf.get_feature_names_out()))
+
+print(cosine_similarity(tfidf_matrix[0:1], tfidf_matrix))
